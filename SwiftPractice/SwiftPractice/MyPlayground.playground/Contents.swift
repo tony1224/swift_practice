ede4c10029d39ff8b18fc9966f99ea4056dfac3e
jun.morita <jun.morita@gree.net>
2023-03-17 23:37:05 +0900

    add bubble sort

--- SwiftPractice/SwiftPractice/MyPlayground.playground/Contents.swift
+++ SwiftPractice/SwiftPractice/MyPlayground.playground/Contents.swift
@@ -1,7 +1,3 @@
-import UIKit
-
-//var greeting = "Hello, playground"
-
 // 全探索
 let (N, v) = (5, 7)
 let a = [4, 3, 12, 7, 11]
@@ -19,4 +15,19 @@ if process() {
     print("YES")
 } else {
     print("NO")
+}
+
+// バブルソート
+var arr = [32,12,12,23,11,19,81,76]
+// 真偽値,swapは交換のこと
+var isSwapped = false
+// 多分今はswift-algorithmsで解決できる
+// ここで一つずつ取り出す
+for index in stride(from: 1, to: arr.count, by: 1) {
+    // 比較
+    if arr[index-1] > arr[index] {
+        // 真であれば数字を入れ替え
+        arr.swapAt((index-1), index)
+        isSwapped = true
+    }
 }
